8c0055bde13ea43ab17f47abfd977d2c1ad398bb
Sahil1607cms <sahil16072004@gmail.com>
2025-08-01 00:26:33 +0530

    backend socket io basics setup

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import http from "http"
+import { Server } from "socket.io";
+const app = express()
+
+const server = http.createServer(app)
+const io = new Server(server)
+const PORT = process.env.PORT || 5001 
+
+io.on("connection" ,(socket)=>{
+    console.log("Socket is connectedd " , socket.id)
+})
+
+server.listen(PORT, ()=>{
+    console.log("Server connected to port ", PORT )
+})
