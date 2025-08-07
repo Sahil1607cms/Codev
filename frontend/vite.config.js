f443614b65c569ae3f9673f07fd781463a619ba2
Sahil1607cms <sahil16072004@gmail.com>
2025-08-07 23:47:58 +0530

    undo the build process

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-  server: {
-    port: 3000,
-  },
+  plugins: [
+    tailwindcss(),
+  ],
 })
